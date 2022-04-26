ebdcce66356cfbaae9da49fb9c4de37c34bfeda5
Zarate25 <zarate_20_@hotmail.com>
2022-04-26 07:27:10 -0500

    pagina web

---
+++ isc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+<body background="img/5.jpg">
+<link href="img/5.jpg" rel="icon"/>
+</head>
+<CENTER/>
+<h1><font color="#FFFFFF">INGENIERIA EN SISTEMAS COMPUTACIONLES .</h1>
+<h2><font color="#FFFFFF">¿QUE ES LA INGENIERIA EN SISTEMAS COMPUTAIONALES?</h2>
+
+<h3><font color="#FFFFFF">La Ingeniería de Sistemas Computacionales es la encargada de desarrollar las aplicaciones o programas (software) basándose en estándares internacionales y en las tecnologías actuales sobre cómputo, microprocesadores y que son de mucha utilidad para el mundo un ingeniero en sistemas computacionales es capaz de producir soluciones y mejoras en los procesos de las organizaciones creando Sistemas Computacionales, Soluciones de Software y Tecnologías de Información..</h3>
+
+<h4><font color="#FFFFFF">¿TODABIA NO TE ANIMAS?.</h4>
+<h5><font color="#FFFFFF">TE MOSTRAMOS ALGUNAS FOTOS Y VIDEOS.</h5>
+   
+           </form>
+<form action="videoI.html" method="post">
+       <input type="submit" value="VIDEOS" id="boton"/>
+      </form> 
+           </form>
+<form action="inicio.html" method="post">
+       <input type="submit" value="REGRESAR" id="boton"/>
+      </form> 
+           </form>
+<form action="index.html" method="post">
+       <input type="submit" value="inicio" id="boton"/>
+      </form> 
+<body>
+
+</body>
+</html>
---
+++ ntc.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Nutricion</title>
+</head>
+<body background="img/4.jpg">
+<link href="img/4.jpg" rel="icon"/>
+</head>
+<CENTER/>
+<h1><font color="#FFFFFF">LICENCIATURA EN NUTRICION.</h1>
+<h2><font color="#FFFFFF">¿QUE ES LA LICENCIATURA EN NUTRICION ?</h2>
+<h3><font color="#FFFFFF">El licenciado en Nutrición es un profesional capaz de brindar atención nutriológica a individuos sanos, en riesgo o enfermos, así como a grupos de los diferentes sectores de la sociedad; administrar servicios y programas de alimentación y nutrición; proponer, innovar y mejorar la calidad nutricional y sanitaria de productos alimenticios.
+
+Es capaz de integrar, generar y aplicar conocimientos, habilidades y actitudes que permiten su desempeño en los campos profesionales básicos: nutrición clínica, nutrición poblacional, servicios de alimentos, ciencia de los alimentos y otros campos transversales, como investigación, educación, administración y consultoría, y aplica métodos, técnicas y tecnologías propias de la nutriología y ciencias afines.
+
+Fundamenta su ejercicio profesional en un marco ético y multidisciplinario para responder con calidad y compromiso a las necesidades sociales de alimentación y nutrición presentes y futuras</h3>
+
+<h4><font color="#FFFFFF">¿TODABIA NO TE ANIMAS?.</h4>
+<h5><font color="#FFFFFF">TE MOSTRAMOS ALGUNAS FOTOS Y VIDEOS.</h5>
+
+           </form>
+<form action="videoN.html" method="post">
+       <input type="submit" value="VIDEOS" id="boton"/>
+      </form> 
+           </form>
+<form action="inicio.html" method="post">
+       <input type="submit" value="REGRESAR" id="boton"/>
+      </form> 
+           </form>
+<form action="index.html" method="post">
+       <input type="submit" value="inicio" id="boton"/>
+      </form> 
+<body>
+</body>
+</html>
